47a82f3d74cadc657e9e6c595cb344590dcb0f45
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-30 15:56:59 +0900

    First Commit

---
+++ QuizApp/QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Kazutomo Kita on 2020/03/30.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
