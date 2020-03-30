05be77e949af2279f8fe5e3d788771d7f75a172f
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-30 20:48:38 +0900

    csvのデータを読み込み

---
+++ QuizApp/QuizApp/ScoreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScoreViewController.swift
+//  QuizApp
+//
+//  Created by Kazutomo Kita on 2020/03/30.
+//  Copyright © 2020 Kazutomo Kita. All rights reserved.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- QuizApp/QuizApp/ViewController.swift
+++ QuizApp/QuizApp/ViewController.swift
@@ -9,10 +9,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var creditLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var csvArray = [String]()
+
+        guard let csvBundle = Bundle.main.path(forResource:"start", ofType:"csv") else {
+            print("csvファイルがないよ")
+            return
+        }
+
+        do {
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            //csvArray = csvData.components(separatedBy: .newlines)
+            //csvArray.removeLast()
+            
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            //csvArray = lineChange.componentsSeparatedByString("\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            
+        } catch let error as NSError {
+            print("エラー: \(error)")
+            return
+        }
+        print(csvArray)
+        
+        logoImageView.image = UIImage(named: csvArray[0])
+        
+        titleLabel.text = csvArray[1]
+        
+        bodyTextView.text = csvArray[2]
+        
+        creditLabel.text = csvArray[3]
+        
+        /*
+        var csvArray:[String] = []
+        
+        let csvBundle = Bundle.main.path(forResource: "start", ofType: "csv")!
+        
+        do {
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.componentsSeparatedByString("\n")
+        }catch{
+            print("エラー")
+        }
+         
+          print(csvArray)
+        */
+        /*
+        var csvArray:[String] = []
+        
+        //csvファイルの読み込み
+        let csvBundle = Bundle.main.path(forResource: "start", ofType: "csv")
+        //エラー値の設定
+        do {
+        //csvBundleのパスを読み込み、UTF8に変換して、NSStringに格納
+            let csvData = try String(contentsOfFile: csvBundle!, encoding: String.Encoding.utf8)
+        //改行コードが"\r"で行われている場合は"\n"にへんこうする
+        let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+        //"n"の改行コードで区切って、配列csvArrayに格納する
+            csvArray = lineChange.components(separatedBy: "\n")
+
+        print(csvArray)
+        */
+       
+      
     }
 
 
