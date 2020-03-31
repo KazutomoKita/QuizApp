85a6ca159f9d195048c1480b9b0ffc778b006a8a
KazutomoKita <elliottstar77@yahoo.co.jp>
2020-03-31 13:07:51 +0900

    選択ボタンアクション設定

--- QuizApp/QuizApp/KenteiViewController.swift
+++ QuizApp/QuizApp/KenteiViewController.swift
@@ -44,15 +44,10 @@ class KenteiViewController: UIViewController {
         answerButton1.setTitle(mondaiArray[5], for: .normal)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonAction(sender: UIButton) {
+        if sender.tag == Int(mondaiArray[1]) {
+            judgeImageView.image = UIImage(named: "maru")
+        }
     }
-    */
 
 }
